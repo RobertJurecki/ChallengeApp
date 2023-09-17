d647af2b51b761cbb8677a261056317ac3ec5658
RobertJurecki <robert.jurecki@tlen.pl>
2023-09-17 14:13:41 +0200

    Zadanie 3

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var imie = "Ewa";
+var płeć = 'k';
+var wiek = 33;
+
+if (płeć == 'k' && wiek < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (imie == "Ewa" && wiek == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (płeć == 'm' && wiek < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("Osoba nie spełniająca parametrów programu");
+}
