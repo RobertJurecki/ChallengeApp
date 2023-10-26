1b1634a5f1a00be2767a5144dd0a82b710560ee8
RobertJurecki <robert.jurecki@tlen.pl>
2023-10-26 16:17:57 +0200

    Zadanie 6

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,20 +1,65 @@
-﻿int number = 4594;
-string numberAsString = number.ToString();
-char[] letters = numberAsString.ToCharArray();
-char[] numbers = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-int[] numberOfDigits = new int[10];
+﻿Employee Employee1 = new Employee("Hans", "Klos", 35);
+Employee Employee2 = new Employee("Janek", "Kos", 18);
+Employee Employee3 = new Employee("Gustlik", "Jeleń", 25);
 
-foreach (char letter in letters)
+Employee1.AddScore(1);
+Employee1.AddScore(2);
+Employee1.AddScore(3);
+Employee1.AddScore(4);
+Employee1.AddScore(5);
+
+Employee2.AddScore(5);
+Employee2.AddScore(4);
+Employee2.AddScore(1);
+Employee2.AddScore(2);
+Employee2.AddScore(1);
+
+Employee3.AddScore(3);
+Employee3.AddScore(3);
+Employee3.AddScore(1);
+Employee3.AddScore(2);
+Employee3.AddScore(3);
+
+List<Employee> Employees = new List<Employee>()
 {
-    for (int i = 0; i < 10; i++)
+    Employee1, Employee2, Employee3 
+};
+
+int maxResult = -1;
+Employee EmployeeWithMaxResult = null;
+
+foreach(var Employee in Employees)
+{
+    if (Employee.Result > maxResult)
     {
-        if (letter == numbers[i]) { numberOfDigits[i]++; }
+        maxResult = Employee.Result;
+        EmployeeWithMaxResult = Employee;
+    }
+        
+}
+Console.WriteLine($"Największą ilość punktów: {maxResult} uzyskał {EmployeeWithMaxResult.Name} {EmployeeWithMaxResult.Surname} lat {EmployeeWithMaxResult.Age}");
+
+class Employee
+{
+    public string Name { get; }
+    public string Surname { get; }
+    public int Age { get; }
+    private int result;
+
+    public int Result
+    {
+        get { return result; }
     }
 
-}
-    Console.WriteLine("Wyniki dla liczby: " + number);
+    public Employee(string name, string surname, int age) 
+    {
+        Name = name;
+        Surname = surname;
+        Age = age;
+    }
 
-for (int i = 0; i < numberOfDigits.Length; i++)
-{
-    Console.WriteLine($"{i} => {numberOfDigits[i]}");
+    public void AddScore(int score)
+    {
+        result += score;
+    }
 }
