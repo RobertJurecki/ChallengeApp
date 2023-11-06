9bde28a9fd5af459ba725d782c660bc202812e96
RobertJurecki <robert.jurecki@tlen.pl>
2023-11-06 19:29:23 +0100

    Zadanie 8

--- ChallengeApp/ChallengeApp/User.cs
+++ ChallengeApp/ChallengeApp/User.cs
@@ -3,6 +3,13 @@
     public class User
     {
     private List<int> score = new List<int>();
+
+        public User(string login)
+        {
+            this.Login = login;
+         
+        }
+
         public User(string login, string password)
         {
             this.Login = login;
