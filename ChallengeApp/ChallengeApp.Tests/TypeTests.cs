95bf0f78a10db0ca644ef116ef3c2945b81eed37
RobertJurecki <robert.jurecki@tlen.pl>
2023-11-06 19:29:13 +0100

    Zadanie 8

---
+++ ChallengeApp/ChallengeApp.Tests/TypeTests.cs
@@ -0,0 +1,49 @@
+﻿namespace ChallengeApp.Tests
+{
+    public class TypeTests
+    {
+        [Test]
+        public void ComperingTwoIntReturnDiferent()
+        {
+            // arrange
+            int number1 = 1;
+            int number2 = 2;
+
+            // act
+
+            // assert
+            Assert.AreNotEqual(number1, number2);
+
+        }
+        [Test]
+        public void ComperingTwoString()
+        {
+            // arrange
+            string number1 = "Janek";
+            string number2 = "Janek";
+
+            // act
+
+            // assert
+            Assert.AreEqual(number1, number2);
+        }
+
+        [Test]
+        public void GetUserShouldReturnDiferent()
+        {
+            // arrange
+            var user1 = GetUser("Adam");
+            var user2 = GetUser("Adam");
+
+            // act
+
+            // assert
+            Assert.AreNotEqual(user1, user2);
+        }
+
+        private User GetUser(string name)
+        {
+            return new User(name);
+        }
+    }
+}
